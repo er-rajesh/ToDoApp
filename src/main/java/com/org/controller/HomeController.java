97f38d222a05e49c4856fdc00cbebc3e6f23aa9a
er-rajesh <brainforce.git@gmail.com>
2022-05-22 22:49:59 +0530

    Update HomeController.java

--- src/main/java/com/org/controller/HomeController.java
+++ src/main/java/com/org/controller/HomeController.java
@@ -1,15 +1,25 @@
 package com.org.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/home")
-	public String home() {
-		System.out.println("In Home Controller /home");
-		return "home";
+	public String home(Model model) {
+		String str="home";
+		System.out.println("/home in controller");
+		model.addAttribute("page", str);
+		return str;
+	}
+
+	public String add(Model model) {
+		String str ="add";
+		System.out.println("/add in controller");
+		model.addAttribute("page",str);
+		return str;
 	}
 
 }
